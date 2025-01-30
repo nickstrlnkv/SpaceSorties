fd50c5b1e6de166948a3d21c5c4d9a658f64a8a2
Nikita <98941217+nickstrlnkv@users.noreply.github.com>
2025-01-30 22:33:47 +0500

    Написал метод для получения размера диска

--- SpaceSorties.CLI/Program.cs
+++ SpaceSorties.CLI/Program.cs
@@ -6,9 +6,32 @@ namespace SpaceSorties.CLI
     {
         static void Main(string[] args)
         {
-            SpaceSortiesTools spaceSortiesTools = new SpaceSortiesTools();
+            /*
+            Tools spaceSortiesTools = new Tools();
 
             spaceSortiesTools.PrintAvailableDrives();
+
+            DriveInfo[] driveinfo = spaceSortiesTools.AllDrives;
+
+            Console.WriteLine(driveinfo[0].Name);
+            foreach (string folder in Directory.GetDirectories(driveinfo[0].Name))
+            {
+                Console.WriteLine(folder);
+            }
+
+            long result = spaceSortiesTools.GetDirectorySize(driveinfo[0].Name);
+            result = driveinfo[0].TotalSize;
+            Console.WriteLine($"Directory: {driveinfo[0].Name} {Tools.FormatSize(result)}");
+            */
+
+            Tools spaceSortiesTools = new Tools();
+            spaceSortiesTools.PrintAvailableDrives();
+            DriveInfo[] driveinfo = spaceSortiesTools.AllDrives;
+            DriveInfo diskE = driveinfo[2];
+            Console.WriteLine(diskE.Name);
+            Console.WriteLine(Tools.FormatSize(Tools.GetDiskSize(diskE.Name)));
+            Console.WriteLine(Tools.FormatSize(Tools.GetDirectorySizeNoEnumerate(diskE.Name)));
+
         }
     }
 }
---
+++ SpaceSorties.Core/Tools.cs
@@ -0,0 +1,130 @@
+﻿namespace SpaceSorties.Core
+{
+    /// <summary>
+    /// Инструментарий для работы с библиотекой
+    /// </summary>
+    public class Tools
+    {
+        private DriveInfo[] allDrives;
+        public DriveInfo[] AllDrives
+        {
+            get
+            {
+                return allDrives;
+            }
+        }
+
+        public Tools()
+        {
+            allDrives = DriveInfo.GetDrives();
+        }
+
+        /// <summary>
+        /// Вывести на консоль доступные диски
+        /// </summary>
+        public void PrintAvailableDrives()
+        {
+            foreach (DriveInfo drive in allDrives)
+            {
+                Console.WriteLine($"Drive: {drive.Name}");
+            }
+        }
+
+        
+        public static long GetDirectorySizeNoEnumerate(string path)
+        {
+            try
+            {
+                Directory.GetFiles(path);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 0;
+            }
+            string[] files = Directory.GetFiles(path);
+            string[] folders = Directory.GetDirectories(path);
+            long totalBytes = 0;
+            FileInfo info;
+            foreach (string file in files)
+            {
+                info = new FileInfo(file);
+                totalBytes += info.Length;
+            }
+            
+            if (folders.Length != 0)
+            {
+                foreach (string folder in folders)
+                {
+                    totalBytes += GetDiskSize(folder);
+                }
+                return totalBytes;
+            }
+            return totalBytes;
+        }
+        
+        /// <summary>
+        /// Получить размер директории, например 'C:\'
+        /// </summary>
+        /// <param name="path">Путь к директории</param>
+        /// <returns>Возвращает размер директории в битах</returns>
+        public static long GetDiskSize(string path)
+        {
+            try
+            {
+                long totalBytes = 0;
+
+                foreach (var file in Directory.EnumerateFiles(path))
+                {
+                    try
+                    {
+                        totalBytes += new FileInfo(file).Length;
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        // Пропускаем файлы, к которым нет доступа
+                        continue;
+                    }
+                }
+                foreach (var folder in Directory.EnumerateDirectories(path))
+                {
+                    try
+                    {
+                        totalBytes += GetDiskSize(folder);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        // Пропускаем директории, к которым нет доступа
+                        continue;
+                    }
+                }
+                return totalBytes;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Пропускаем текущую директории, если к ней нет доступа
+                return 0;
+            }
+        }
+        
+        /// <summary>
+        /// Получить размер файла, директории в привычном виде, например '5.00MB', '22.05 GB'.
+        /// </summary>
+        /// <param name="size">размер в битах</param>
+        /// <returns>Возвращает размер в отформатированом виде.</returns>
+        public static string FormatSize(long size)
+        {
+            if (size >= 1024 * 1024 * 1024)
+                return $"{size / (1024.0 * 1024 * 1024):F2} GB";
+
+            else if (size >= 1024 * 1024)
+                return $"{size / (1024.0 * 1024):F2} MB";
+
+            else if (size >= 1024)
+                return $"{size / 1024.0:F2} KB";
+            else
+                return $"{size} bytes";
+        }
+
+    }
+}
+
